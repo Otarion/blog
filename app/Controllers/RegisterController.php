cc5432c59cbbf7adae54d454fe685e3b816c9bd3
Otarion <c.garsault@laposte.net>
2024-02-09 14:29:39 +0100

    La page d'inscription (correction)

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -2,20 +2,9 @@
 
 namespace App\Controllers;
 
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-
 class RegisterController extends Controller
 {
-    private $twig;
-
-    public function __construct()
-    {
-        $loader = new FilesystemLoader;
-        $this->twig = new Environment($loader);
-    }
-
     public function showRegisterForm() {
-        echo $this->twig->render('register.html', []);
+        return $this -> view ('register');
     }
 }
